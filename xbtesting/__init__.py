8e04fa7b8b32191c29c0342a0280fbf8d0759658
Philipp Bosch <hello@pb.io>
2011-02-09 16:47:37 +0100

    Bump version number.

--- xbtesting/__init__.py
+++ xbtesting/__init__.py
@@ -1,6 +1,6 @@
 from .xbtesting import XBTesting, XBTest, XBTestVersion, XBTestVersionResult
 
-VERSION = ('0', '1', '2')
+VERSION = ('0', '1', '3')
 
 __version__ = ".".join(VERSION)
 __all__ = ['XBTesting', 'XBTest', 'XBTestVersion', 'XBTestVersionResult']
